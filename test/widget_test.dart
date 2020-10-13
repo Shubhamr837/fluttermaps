09b19a933d5ac06f5196565b0be37b969e4a8716
shubhamr837 <shubhamr837@gmail.com>
2020-10-13 10:25:07 +0530

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fluttermapsapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MapsDemo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
